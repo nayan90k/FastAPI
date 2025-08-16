43d73defcbc4f746e1ea0b5fd3b88ea82e085799
kamalnayan <kamalnayanofficialwork@gmail.com>
2025-08-16 21:49:34 +0530

    pydantic type validation

---
+++ pydantic_why.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+class Patient(BaseModel):
+    name: str
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print("inserted")
+
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print("updated")
+
+patient_info = {"name": "kamalnayan", "age": '30', 'weight': 75.2, 'married': True, 'allergies':['pollen', 'dust'], 'contact_details': {'email': 'abc@gmail.com', 'phone':'6278346237'}}
+
+patient1 = Patient(**patient_info)
+
+update_patient_data(patient1)

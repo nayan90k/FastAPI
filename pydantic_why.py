84e31a1da3c76a04baca0f0a80f1c7ae549ff1ee
kamalnayan <kamalnayanofficialwork@gmail.com>
2025-08-16 22:07:19 +0530

    data validation start

--- pydantic_why.py
+++ pydantic_why.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
+    email: EmailStr
+    linkedin_url: AnyUrl
     age: int
     weight: float
     married: bool = False
@@ -17,12 +19,13 @@ def insert_patient_data(patient: Patient):
 
 def update_patient_data(patient: Patient):
     print(patient.name)
+    print(patient.email)
     print(patient.age)
     print(patient.allergies)
     print(patient.married)
     print("updated")
 
-patient_info = {"name": "kamalnayan", "age": '30', 'weight': 75.2, 'contact_details': {'email': 'abc@gmail.com', 'phone':'6278346237'}}
+patient_info = {"name": "kamalnayan", 'email': 'abc@gmail.com', 'linkedin_url':'http://linkedin.com/2323',"age": '30', 'weight': 75.2, 'contact_details': {'phone':'6278346237'}}
 
 patient1 = Patient(**patient_info)
 
